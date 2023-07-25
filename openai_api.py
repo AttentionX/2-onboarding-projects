ea76ad326a93e4663c25f037acc9d49faebe3bce
Adam <cmlee3211@gmail.com>
2023-07-26 02:33:15 +0900

    Use GPT-4 for natbot

--- openai_api.py
+++ openai_api.py
@@ -12,17 +12,17 @@ model = "gpt-3.5-turbo"
 system_message = "You are a helpful assistant"
 query = "Explain self-attention"
 
-class OpenAIAPI:
+class OpenAI_API:
     def __init__(self, model=model, system_message=system_message):
         self.model = model
         self.system_message = system_message
 
-    def get_response(self, query):
+    def chatgpt(self, query):
         messages = [
             {"role":"system", "content":self.system_message},
             {"role":"user", "content":query}
         ]
-        response = openai.ChatCompletion.create(model=self.model, messages=messages).choices[0].message
+        response = openai.ChatCompletion.create(model=self.model, messages=messages).choices[0].message.content
         return response
     
     @staticmethod
--- week2/README.md
+++ week2/README.md
@@ -1,5 +1,16 @@
 # 2. Browsing Agent
 
+## To-do
+1. Install dependencies
+```bash
+cd season2-onboarding-projects/week2
+pip3 install -r requirements.txt
+```
+2. Setup OpenAI API
+```python
+
+```
+
 ## Examples
 1. [natbot](https://github.com/nat/natbot/tree/main)
   - October 2022
@@ -7,8 +18,9 @@
 ## Example Tasks
 1. Browsing Automation
     1. 다음주 금요일에 강남역 CGV에서 오후 8시 인근에 인디아나존스 2명 예약해줘
+        - CGV 계정 등 필요한 정보 prompt로 알려주거나 Bot이 유저한테 물어보도록 하기
     2. 배민에서 지금 강남역 인근 피자 맛있는거 배달해줘
-        - 추가 정보 물어보는지 확인
+        - 태스크에 필요한 추가 정보가 있으면 물어보는지 확인
             - 어디로 배달하면 될까요?
             - 피쟈집 / 피자 종류 이 중에서 선택해주세요
 3. General Problem Solving
